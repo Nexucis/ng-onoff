40a7e5afa9b666c6b906a0e47bd64910e0b1c52c
Célian GARCIA <celian.garcia1@gmail.com>
2021-11-26 14:35:01 +0100

    Upgrade Angular to v13 (#23)
    * Upgrade Angular to v13
    
    * Use node 16 build image
    
    * Enable default angular linter (angular-eslint)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,10 @@ import { AppComponent } from './app.component';
 import { NgOnoffModule } from '../../projects/onoff/src/lib/ng-onoff.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OnoffDialogComponent } from './onoff-dialog/onoff-dialog.component';
-import { MatButtonModule, MatDialogModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { A11yModule } from '@angular/cdk/a11y';
 
 @NgModule({
